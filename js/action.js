4ffb0c7fc90959a7e4462767d94ead60049fcd95
陈海峰 <chenhaifeng@hualala.com>
2019-01-07 10:42:13 +0800

    ok

--- js/action.js
+++ js/action.js
@@ -2,12 +2,11 @@
  * 哗啦啦抽奖小程序
  * 抽中名字后该字符串即删掉。
  */
+let mytime = null;
+let selected=nameList.length;
 
-var mytime = null;
-var selected=nameList.length;
-
-function doit() {
-    var bt = window.document.getElementById("bt");
+const doit=() => {
+    let bt = document.querySelector("#bt");
     if (mytime == null) {
         bt.innerHTML = "停止抽奖";
         show();
@@ -15,19 +14,22 @@ function doit() {
         bt.innerHTML = "开始抽奖";
         clearTimeout(mytime);
         nameList.splice(selected,1);
+        if(nameList.length===0){
+            nameList=['哗啦啦']
+        }
         mytime = null;
     }
 }
 
-function show() {
-    var box = window.document.getElementById("box");
-    var num = Math.floor( Math.random() * nameList.length );
+const show = ()=> {
+    let box = window.document.getElementById("box");
+    let num = Math.floor( Math.random() * nameList.length );
     box.innerHTML = nameList[num];
     mytime = setTimeout("show()", 1);
     selected=num;
 }
 
-function spaceClick(e) {
+const spaceClick =(e) => {
     var keynum = window.event ? e.keyCode : e.which;
     if(keynum=='32'){
         doit();
