f2ee3f95ec4d8591e0a9bcbd637858bb6b403ee7
陈海峰 <chenhaifeng@hualala.com>
2019-01-07 10:00:49 +0800

    init

---
+++ js/action.js
@@ -0,0 +1,37 @@
+/**
+ * 哗啦啦抽奖小程序
+ * 抽中名字后该字符串即删掉。
+ */
+
+var mytime = null;
+var selected=nameList.length;
+
+function doit() {
+    var bt = window.document.getElementById("bt");
+    if (mytime == null) {
+        bt.innerHTML = "停止抽奖";
+        show();
+    } else {
+        bt.innerHTML = "开始抽奖";
+        clearTimeout(mytime);
+        nameList.splice(selected,1);
+        mytime = null;
+    }
+}
+
+function show() {
+    var box = window.document.getElementById("box");
+    var num = Math.floor((Math.random() * 7410852963)) % nameList.length;
+    box.innerHTML = nameList[num];
+    mytime = setTimeout("show()", 1);
+    selected=num;
+}
+
+function spaceClick(e) {
+    var keynum = window.event ? e.keyCode : e.which;
+    if(keynum=='32'){
+        doit();
+    }
+}
+
+window.addEventListener('keydown',spaceClick)
