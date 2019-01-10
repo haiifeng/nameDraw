650332052a66fa5b377a7ba32526a000a49e02cc
陈海峰 <chenhaifeng@hualala.com>
2019-01-10 15:14:26 +0800

    开始3秒内不能再次点击，结束三秒内不能再点击

--- js/action.js
+++ js/action.js
@@ -5,15 +5,17 @@
 let mytime = null;
 let nameList=Array.from(Array(400), (v,k) =>k);
 let selected=nameList.length;
+let timer=null,flag=true;
+let bt = document.querySelector("#bt"); 
 
-const doit=() => {
-    let bt = document.querySelector("#bt");
+const doit=() => {  
     if (mytime == null) {
         bt.innerHTML = "停止抽奖";
         show();
     } else {
         bt.innerHTML = "开始抽奖";
         clearTimeout(mytime);
+        clearTimeout(timer);
         nameList.splice(selected,1);
         if(nameList.length===0){
             nameList=['哗啦啦']
@@ -26,15 +28,18 @@ const show = ()=> {
     let box = document.querySelector("#box");
     let num = Math.floor( Math.random() * nameList.length );
     box.innerHTML = nameList[num];
-    console.log(num,nameList);
     mytime = setTimeout("show()", 1);
     selected=num;
 }
 
 const spaceClick =(e) => {
-    var keynum = window.event ? e.keyCode : e.which;
-    if(keynum=='32'){
-        doit();
+    timer=setTimeout(function(){flag=true;},3000);
+    if(flag){
+        let keynum = window.event ? e.keyCode : e.which;
+        if(keynum=='32'){
+            doit();
+            flag=false;
+        }
     }
 }
 
